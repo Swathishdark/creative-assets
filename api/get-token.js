80976190e0355eef2fb094e52b7cb0fd5ca2c0d9
Ritwik <43515034+Ritwik-28@users.noreply.github.com>
2024-06-09 18:24:25 +0530

    Update get-token.js

--- api/get-token.js
+++ api/get-token.js
@@ -1,29 +1,30 @@
 const fetch = require('node-fetch');
 
 const directusApiEndpoint = process.env.REACT_APP_DIRECTUS_API_ENDPOINT;
+const directusUsername = process.env.DIRECTUS_USERNAME;
+const directusPassword = process.env.DIRECTUS_PASSWORD;
 
 module.exports = async (req, res) => {
-  const { token, program } = req.query;
-
-  if (!token) {
-    return res.status(400).json({ error: 'Token is required' });
-  }
-
   try {
-    const response = await fetch(`${directusApiEndpoint}/items/shareable_assets?filter[program_name][_contains]=${program}&filter[status][_eq]=published`, {
+    const response = await fetch(`${directusApiEndpoint}/auth/login`, {
+      method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+        email: directusUsername,
+        password: directusPassword,
+      }),
     });
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch content: ${response.statusText}`);
+      throw new Error(`Failed to login: ${response.statusText}`);
     }
 
     const data = await response.json();
-    res.status(200).json(data.data);
+    res.status(200).json({ token: data.data.access_token });
   } catch (error) {
-    console.error('Error fetching content:', error.message);
+    console.error('Error fetching access token:', error);
     res.status(500).json({ error: error.message });
   }
 };
